f952205e52f7eaab058d582cfc4a0972a8a82c9c
Kang-min Liu <gugod@gugod.org>
2019-12-23 09:54:58 +0900

    order matters.

--- cpanm-install-notest-recent.sh
+++ cpanm-install-notest-recent.sh
@@ -13,7 +13,7 @@ do
     echo
     echo "## $disturl"
 
-    cpanm --notest -L $dist_locallib $disturl 2>&1 > $dist_locallib.log
+    cpanm --notest -L $dist_locallib $disturl 1>$dist_locallib.log 2>&1
     rc=$?
 
     if [[ $rc -eq 0 ]]
--- cpanm-install-recent.sh
+++ cpanm-install-recent.sh
@@ -13,7 +13,7 @@ do
     echo
     echo "## $disturl"
 
-    cpanm -L $dist_locallib $disturl 2>&1 > $dist_locallib.log
+    cpanm -L $dist_locallib $disturl 1>$dist_locallib.log 2>&1
     rc=$?
 
     if [[ $rc -eq 0 ]]
