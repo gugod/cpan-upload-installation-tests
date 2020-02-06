b361b14a3d15ed597ccb06aa94b48a9cfc8e4fe9
Kang-min Liu <gugod@gugod.org>
2020-02-06 22:29:27 +0900

    setup author name

--- cpanm-install-notest-recent.sh
+++ cpanm-install-notest-recent.sh
@@ -24,14 +24,14 @@ do
         echo ok '#' cpanm $disturl
 
         echo '##' Sent to Feedro
-        curl --silent https://gugod.org/feed/CPAN-installation-with-cpanm-notest/items -X POST -H "Authentication: Bearer ${FEEDRO_TOKEN_CPANM_NOTEST}" -F id="$disturl" -F title="SUCCESS $distname" -F content_text='@'${dist_locallib}.logtail
+        curl --silent https://gugod.org/feed/CPAN-installation-with-cpanm-notest/items -X POST -H "Authentication: Bearer ${FEEDRO_TOKEN_CPANM_NOTEST}" -F id="$disturl" -F title="SUCCESS $distname" -F content_text='@'${dist_locallib}.logtail -F "author.name=$FEEDRO_AUTHOR_NAME"
         echo
     else
         fail=1
         echo not ok '#' cpanm $disturl
 
         echo '##' Sent to Feedro
-        curl --silent https://gugod.org/feed/CPAN-installation-with-cpanm-notest/items -X POST -H "Authentication: Bearer ${FEEDRO_TOKEN_CPANM_NOTEST}" -F id="$disturl" -F title="FAIL $distname" -F content_text='@'${dist_locallib}.logtail
+        curl --silent https://gugod.org/feed/CPAN-installation-with-cpanm-notest/items -X POST -H "Authentication: Bearer ${FEEDRO_TOKEN_CPANM_NOTEST}" -F id="$disturl" -F title="FAIL $distname" -F content_text='@'${dist_locallib}.logtail -F "author.name=$FEEDRO_AUTHOR_NAME"
         echo
     fi
 
